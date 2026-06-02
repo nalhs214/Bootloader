c71d73fd18c3579fa4cae1ce7e2a40ff7b78685d
nalhs214 <nalhs214@gmail.com>
2026-06-02 18:16:29 +0900

    bootloader_lunux_MCU
    MCU랑 Linux 통신해보기

---
+++ 03. Bootloader/4) MCU to linux/4. MCU to linux/library.c
@@ -0,0 +1,16 @@
+/*
+ * 4. MCU to linux.c
+ *
+ * Created: 2026-06-02 오전 8:52:18
+ * Author : USER
+ */
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+    return 0;
+}
+
---
+++ 03. Bootloader/4) MCU to linux/4. MCU to linux/main.c
@@ -0,0 +1,44 @@
+/*
+ * 4. MCU to linux.c
+ *
+ * Created: 2026-06-02 오전 8:52:17
+ * Author : USER
+ * MCU 에서 Linux로 데이터를 보내고, 받고를 할 수 있는지 테스트
+ */
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+
+#include "uart.h"
+
+int main(void)
+{
+	UART_init();
+	
+	while(1)
+	{
+
+        unsigned char c = USART_Receive();
+
+        USART_Transmit(c);
+		USART_Transmit('\n');
+		USART_Transmit('\r');
+
+/*
+		USART_Transmit('H');
+		USART_Transmit('e');
+		USART_Transmit('l');
+		USART_Transmit('l');
+		USART_Transmit('o');
+		USART_Transmit('\n');
+		USART_Transmit('\r');
+		USART_Transmit('y');
+		USART_Transmit('\n');
+		USART_Transmit('\r');
+		_delay_ms(1000);		
+*/
+
+	}
+}
+
